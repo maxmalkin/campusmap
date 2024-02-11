2b05e2faefc565cda81ec6f9c176f6f2c5bedb6d
maxmalkin <maxim_malkin@outlook.com>
2024-02-11 15:50:59 -0800

    added map labels

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
+
+//api token:
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4bWFsa2luIiwiYSI6ImNsczVpamxlODFoaG0ycnBhdzd1bTY4amMifQ.Rbe4ueQeIAE6AzCCbtIpqQ';
+
 
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-122.197772, 47.758864], // UW Bothell campus coordinates
-    zoom: 15,
+    center: [-122.191392, 47.759708], //map center
+    zoom: 17,
+    pitch: 6,
     maxBounds: [
-        [-122.209049, 47.752314], // Southwest bound
-        [-122.186492, 47.765457]  // Northeast bound
+        [-122.196920, 47.757326], // SW bound
+        [-122.187897, 47.763077]  // NE bound
     ],
 });
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
 
 map.on('load', () => {
-    // Add Mapbox GL navigation control
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-    // Add a marker for UW Bothell campus
-    new mapboxgl.Marker()
-        .setLngLat([-122.1911463, 47.758586])
-        .addTo(map);
-
-    // Add styled labels for building names
+    
     map.addLayer({
         id: 'building-labels',
         type: 'symbol',
@@ -36,10 +34,59 @@ map.on('load', () => {
                         },
                         geometry: {
                             type: 'Point',
-                            coordinates: [-122.1911463, 47.758586], 
+                            coordinates: [-122.191351, 47.758687], 
                         },
                     },
-                    // Add more buildings as needed
+                    {
+                    type: 'Feature',
+                    properties: {
+                        name: 'Discovery Hall \n (DISC)',
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [-122.191931 ,47.759038]
+                    }
+                    },
+                    {
+                        type: 'Feature',
+                        properties: {
+                            name: 'UW 1',
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [-122.190643, 47.758663]
+                        }
+                    },
+                    {
+                        type: 'Feature',
+                        properties: {
+                            name: 'Library \n (LB)',
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [-122.191434, 47.759829]
+                        }
+                    },
+                    {
+                        type: 'Feature',
+                        properties: {
+                            name: 'Activities \n& Recreation \nCenter \n(ARC)',
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [-122.190274, 47.760042]
+                        }
+                    },
+                    {
+                        type: 'Feature',
+                        properties: {
+                            name: 'Library \n (LB)',
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [-122.191434, 47.759829]
+                        }
+                    },
                 ],
             },
         },
@@ -49,7 +96,13 @@ map.on('load', () => {
             'text-anchor': 'top',
         },
         paint: {
-            'text-color': '#FFD700', // Gold text color
+            'text-color': '#FFD700', 
         },
     });
 });
+
+
+// Add a marker for UW Bothell campus
+// new mapboxgl.Marker()
+//     .setLngLat([-122.1911463, 47.758586])
+//     .addTo(map);
