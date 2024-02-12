88569cfc4dd22b1da666c9ca75c29ac0627c4ff0
maxmalkin <maxim_malkin@outlook.com>
2024-02-11 17:09:00 -0800

    added iterative building labeling, header buttons and animations

--- app.js
+++ app.js
@@ -2,7 +2,61 @@
 //api token:
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4bWFsa2luIiwiYSI6ImNsczVpamxlODFoaG0ycnBhdzd1bTY4amMifQ.Rbe4ueQeIAE6AzCCbtIpqQ';
 
+//builings json
+const buildings = [
+    {
+        "name": "UW 2",
+        "latitude": 47.758687,
+        "longitude": -122.191351,
+    },
+    {
+        "name": "Discovery Hall \n(DISC)",
+        "latitude": 47.759038,
+        "longitude": -122.191931,
+    },
+    {
+        "name": "UW 1",
+        "latitude": 47.758663,
+        "longitude": -122.190643,
+    },
+    {
+        "name": "Library \n(LB)",
+        "latitude": 47.759829,
+        "longitude": -122.191434,
+    },
+    {
+        "name": "Activities \n& Recreation \nCenter \n(ARC)",
+        "latitude": 47.759948,
+        "longitude": -122.190274,
+    },
+    {
+        "name": "CC 1",
+        "latitude": 47.760735,
+        "longitude": -122.191509,
+    },
+    {
+        "name": "CC 2",
+        "latitude": 47.761226,
+        "longitude": -122.191869,
+    },
+    {
+        "name": "CC 3",
+        "latitude": 47.761193,
+        "longitude": -122.192626,
+    },
+    {
+        "name": "North Creek \nEvent Center \n(NCEC)",
+        "latitude": 47.760299,
+        "longitude": -122.190565,
+    },
+    {
+        "name": "Innovation \nHall \n(INV)",
+        "latitude": 47.760400,
+        "longitude": -122.192551,
+    }
+]
 
+//map init
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
@@ -14,11 +68,53 @@ const map = new mapboxgl.Map({
         [-122.187897, 47.763077]  // NE bound
     ],
 });
-map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
 map.on('load', () => {
     
+    //remove default layers by iteration
+    var layers = map.getStyle().layers;
+    layers.forEach(function(layer) {
+        if (layer.type === 'symbol' && layer.layout['text-field']) {
+            map.setLayoutProperty(layer.id, 'visibility', 'none');
+        }
+    });
+
+    //add building labels from buildings object
+    map.addSource('buildings', {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: buildings.map(building => ({
+                type: 'Feature',
+                properties: {
+                    name: building.name,
+                },
+                geometry: {
+                    type: 'Point',
+                    coordinates: [building.longitude, building.latitude],
+                },
+            })),
+        },
+    });
+    map.addLayer({
+        id: 'building-labels',
+        type: 'symbol',
+        source: 'buildings',
+        layout: {
+            'text-field': ['get', 'name'],
+            'text-size': 12,
+            'text-anchor': 'center',
+        },
+        paint: {
+            'text-color': '#FFD700',
+        },
+    });
+});
+
+/* MANUAL LABELS
+    // add map layer for building labels
     map.addLayer({
         id: 'building-labels',
         type: 'symbol',
@@ -74,7 +170,7 @@ map.on('load', () => {
                         },
                         geometry: {
                             type: 'Point',
-                            coordinates: [-122.190274, 47.760042]
+                            coordinates: [-122.190274, 47.759948]
                         }
                     },
                     {
@@ -93,13 +189,13 @@ map.on('load', () => {
         layout: {
             'text-field': ['get', 'name'],
             'text-size': 12,
-            'text-anchor': 'top',
+            'text-anchor': 'center',
         },
         paint: {
             'text-color': '#FFD700', 
         },
     });
-});
+    */
 
 
 // Add a marker for UW Bothell campus
