1bf7f7c3f91c1147f42669e751b559f01df4758a
maxmalkin <maxim_malkin@outlook.com>
2024-02-12 15:58:58 -0800

    added comments

--- app.js
+++ app.js
@@ -131,23 +131,30 @@ map.on('load', () => {
 
 });
 
+//page switching
 document.addEventListener('DOMContentLoaded', function () {
-    // Add click event listener to the document
     document.addEventListener('click', function (event) {
-        // Check if the clicked element is a button with the class 'button'
         if (event.target.matches('.button')) {
-            // Redirect to main.html if the home button is clicked
             if (event.target.textContent === 'Home') {
                 window.location.href = 'main.html';
             }
-            // Redirect to directorypage.html if the directory button is clicked
             else if (event.target.textContent === 'Directory') {
                 window.location.href = 'directorypage.html';
             }
-            // Add conditions for other buttons if needed
         }
     });
 });
+
+
+
+
+
+
+
+
+
+
+
 
 /* 
 // old 
